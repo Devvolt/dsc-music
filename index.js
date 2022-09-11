7d1ac890148baf1416ceb786d36dfc282c6f3e6d
Guglielmo Bragato <guglielmo.bragato@gmail.com>
2022-09-11 15:29:56 +0200

    Co-authored-by: Joele Buffolo <buffolollo@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
-  }
-
-
-exports.print = function(msg) {
-    console.log(msg)
-}
+const ytdl = require("discord-ytdl-core");
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayer,
+  AudioResource,
+} = require("@discordjs/voice");
+exports.play = async function (message) {
+  const stream = ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ", {
+    filter: "audioonly",
+    quality: "highestaudio",
+    highWaterMark: 1 << 25,
+    opusEncoded: true,
+  });
+  const channel = message.member.voice.channel;
+  const connection = await joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+  const player = createAudioPlayer();
+  const resource = createAudioResource(stream, { inlineVolume: true });
+  player.play(resource);
+  connection.subscribe(player);
+};
