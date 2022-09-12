5b9a4268de8787309e311deeb697632d3d630949
Joele Buffolo <elia.stef006@gmail.com>
2022-09-12 14:34:23 +0200

    new updates

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const ytdl = require("ytdl-core");
 const fetch = require("isomorphic-unfetch");
 const spotify = require("spotify-url-info")(fetch);
 const searcher = require("youtube-sr").default;
+const discord = require("discord.js");
+const SC = require("soundcloud-scraper");
+const soundcloud = new SC.Client();
 const spotifyPlaylistRegex =
   /https?:\/\/(?:embed\.|open\.)(?:spotify\.com\/)(?:playlist\/|\?uri=spotify:playlist:)((\w|-){22})/;
 const spotifySongRegex =
@@ -18,18 +21,29 @@ const {
 /* START MUSIC FUNCTIONS */
 
 module.exports = {
+  /**
+   *
+   * @param {discord.Message} message
+   * @param {String[]} query
+   */
   play: async function (message, query) {
+    let trackToPlay;
+    if (query.match(spotifySongRegex)) {
+    }
+    if (query.match(spotifyPlaylistRegex)) {
+      return this._spotifyPlaylist(query);
+    }
     if (searcher.validate(query, "VIDEO")) {
       const songInfo = ytdl.getBasicInfo(query);
       if (!songInfo) throw new Error("No data of the video found");
     }
-
     {
       const result = await searcher.search(query, { limit: 1, type: "video" });
       if (!result || result.length < 1) throw new Error("No videos found!");
     }
   },
-  search: function (message, query) {},
+  _searchTracks: function (query) {},
+  _spotifyPlaylist: function (query) {},
 };
 
 /* END MUSIC FUNCTIONS */
